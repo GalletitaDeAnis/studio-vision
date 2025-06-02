e7bc85afffdac8950713284934ca4cc2d0a5f7ab
GalletitaDeAnis <paolopizarro685@gmail.com>
2025-06-02 00:27:17 -0400

    Contenido pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+// src/app/about/page.tsx
+export default function AboutPage() {
+    return (
+      <main>
+        <h1>Sobre Nosotros</h1>
+        <p>Bienvenido a la página Sobre Nosotros.</p>
+      </main>
+    );
+  }
+  
