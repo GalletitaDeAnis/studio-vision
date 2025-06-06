737fb21faef287808347b8ff8972578876d80f10
GalletitaDeAnis <paolopizarro685@gmail.com>
2025-06-05 22:57:45 -0400

    Fix ESLint error with gtag type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,15 +4,6 @@ export {};
 
 declare global {
   interface Window {
-    /**
-     * Google Analytics gtag function
-     * Example usage:
-     * gtag('event', 'click', { event_category: 'video' });
-     */
-    gtag: (
-      command: 'config' | 'event' | 'set',
-      target: string,
-      options?: Record<string, unknown>
-    ) => void;
+    gtag: (...args: unknown[]) => void;
   }
 }
